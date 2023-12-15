2e8e781f0aba4a099ddc3b67dfbdc1a137ac9404
sohyun <shlim0410@gmail.com>
2023-12-15 23:05:14 +0000

    day15 part1 done

---
+++ day15/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(__dirname, "input.txt");
+const input = fs.readFileSync(filePath).toString().split(",");
+
+
+function getHashed(str) {
+  let curr = 0;
+  for (let i = 0; i < str.length; i++) {
+    curr += str[i].charCodeAt(0);
+    curr *= 17;
+    curr %= 256;
+  }
+
+  return curr;
+}
+
+const res = input
+  .map((str) => getHashed(str))
+  .reduce((acc, curr) => acc + curr, 0);
+
+console.log(res);
