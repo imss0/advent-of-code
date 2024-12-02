cfa18101126c2755af1a34b81a6539ba4d7dba15
sohyun <shlim0410@gmail.com>
2024-12-02 23:37:11 +0000

    day2 part2

--- day2_2024/index.js
+++ day2_2024/index.js
@@ -41,13 +41,29 @@ function isDiffInRange(arr) {
 }
 
 let cnt = 0;
+let newCnt = 0;
 for (let i = 0; i < input.length; i++) {
   if (
     (isAllIncreasing(input[i]) || isAllDecreasing(input[i])) &&
     isDiffInRange(input[i])
   ) {
     cnt++;
+  } else {
+    const newSafe = [];
+    for (let j = 0; j < input[i].length; j++) {
+      const newArr = input[i].slice(0, j).concat(input[i].slice(j + 1));
+      if (
+        (isAllIncreasing(newArr) || isAllDecreasing(newArr)) &&
+        isDiffInRange(newArr)
+      ) {
+        newSafe.push(newArr);
+      }
+    }
+    if (newSafe.length > 0) {
+      newCnt++;
+    }
   }
 }
 
 console.log(cnt);
+console.log(newCnt + cnt);
