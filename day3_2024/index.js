571885ae6b0c06ef51d78658a597ab8f084e274d
sohyun <shlim0410@gmail.com>
2024-12-03 23:21:05 +0000

    part2 done

--- day3_2024/index.js
+++ day3_2024/index.js
@@ -3,13 +3,17 @@ const path = require("path");
 const filePath = path.join(__dirname, "input.txt");
 const input = fs.readFileSync(filePath).toString().trim();
 
-const regex = /mul\((\d{1,3}),(\d{1,3})\)/g;
-
+const regex = /don't()|do()|mul\((\d{1,3}),(\d{1,3})\)/g;
 const matches = [...input.matchAll(regex)];
 
 let res = 0;
 
+let enabled = true;
 matches.forEach((match) => {
-  res += match[1] * match[2]
+  if (match[0] !== "don't" && match[0] !== "do" && enabled) {
+    res += match[3] * match[4];
+  } else if (match[0] === "don't") enabled = false;
+  else if (match[0] === "do") enabled = true;
 });
-console.log(res)
+
+console.log(res);
